f383d8c3392c0233ca968bd0099162f9a86e20fe
EC2 Default User <ec2-user@ip-172-31-29-181.us-west-2.compute.internal>
2018-01-09 18:57:24 +0000

    changes to stop_server.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g npm"
+sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g npm"
 # npm i -g npm
-su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g @angular\/cli@latest"
+sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g @angular\/cli@latest"
 # npm i -g @angular\/cli@latest
-su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g pm2"
+sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g pm2"
 
