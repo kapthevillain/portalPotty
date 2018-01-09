b8d074f78a61e9423b35a27c2426f8de0847913c
EC2 Default User <ec2-user@ip-172-31-29-181.us-west-2.compute.internal>
2018-01-09 20:07:41 +0000

    changes to stop_server.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /var/www/pottypatrol
+
 sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g npm"
 # npm i -g npm
 sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/npm i -g @angular\/cli@latest"
