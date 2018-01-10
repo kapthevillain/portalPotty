f35d5474d8c27c1aba69e063d01e75e97e852f09
EC2 Default User <ec2-user@ip-172-31-29-181.us-west-2.compute.internal>
2018-01-10 20:10:28 +0000

    changes to stop_script for redeployment

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-npm i -g pm2
-cd /var/www/pottypatrol
-pm2 stop all
+#npm i -g pm2
+#cd /var/www/pottypatrol
+doesAppExist='pgrep nginx'
+if [[ -n $doesAppExist ]]; then
+	service nginx stop
+	pm2 stop all
+fi
 
