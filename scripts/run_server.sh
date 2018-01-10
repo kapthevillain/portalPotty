4df8dd8d0512ecc93499ad5ff3e02ed57fd8925b
EC2 Default User <ec2-user@ip-172-31-29-181.us-west-2.compute.internal>
2018-01-10 19:16:19 +0000

    changes to install_packages

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -2,8 +2,9 @@
 cd /var/www/pottypatrol
 echo $PWD
 
-#su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/ng build"
-#ng build
+# Start services
+sudo service nginx start
 
+# Start node server
 su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.3.0/bin/pm2 start /var/www/pottypatrol/server/server.js"
 #pm2 start server/server.js 
