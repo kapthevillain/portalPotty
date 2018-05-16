c6e2f2fc1484a5080b79c68c092058b4290bd2af
root <andrewkap12@gmail.com>
2018-05-16 19:27:33 +0000

    Edited install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,6 +5,6 @@ echo $PWD
 
 sudo chown ec2-user: /var/www/pottypatrol
 
-su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.0/bin/npm --prefix /var/www/pottypatrol install"
+su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.1/bin/npm --prefix /var/www/pottypatrol install"
 #sudo npm install
-su - ec2-user -c "(cd /var/www/pottypatrol && /home/ec2-user/.nvm/versions/node/v9.*.0/bin/ng build)"
+su - ec2-user -c "(cd /var/www/pottypatrol && /home/ec2-user/.nvm/versions/node/v9.*.1/bin/ng build)"
