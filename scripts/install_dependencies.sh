5eec26f1b409e95b1518786f9f5d3cf06d91ed44
root <andrewkap12@gmail.com>
2018-05-16 19:26:59 +0000

    Edited install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,13 +4,13 @@
 sudo mkdir -p /var/www/pottypatrol
 
 # Install latest NodeJS
-sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.0/bin/npm i -g npm"
+sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.1/bin/npm i -g npm"
 
 # Install Angular
-sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.0/bin/npm i -g @angular\/cli@latest"
+sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.1/bin/npm i -g @angular\/cli@latest"
 
 # Install pm2
-sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.0/bin/npm i -g pm2"
+sudo su - ec2-user -c "/home/ec2-user/.nvm/versions/node/v9.*.1/bin/npm i -g pm2"
 
 # Install NGINX
 sudo yum -y install nginx
